4fec4ce77fb10006e3fe18d6487396762b3ac24f
Araari adem <araariadem0@gmail.com>
2023-04-18 04:06:54 +0200

    add support for 12 new websites

--- hunter/scraping/website_categories.py
+++ hunter/scraping/website_categories.py
@@ -10,6 +10,10 @@ category_1 = [
 	'https://calendar.ku.edu/',
 	'https://calendar.usc.edu/',
 	'https://events.miami.edu/',
+	# Newly added websites
+	'https://events.purdue.edu/',
+	'https://calendar.usc.edu/',
+	'https://calendar.ohio.edu/',
 ]
 
 category_2 = [
@@ -21,6 +25,16 @@ category_2 = [
 	'https://events.bc.edu/',
 	'https://events.trinity.edu/',
 	'https://happenings.wustl.edu/',
+	# New
+	'https://calendar.uoregon.edu/',
+	'https://calendar.oregonstate.edu/',
+	'https://calendar.pitt.edu/',
+	'https://calendar.utexas.edu/',
+	'https://calendar.fsu.edu/',
+	'https://calendar.utk.edu/',
+	'https://calendar.utrgv.edu/',
+	'https://calendar.ucsc.edu/',
+	'https://calendar.colorado.edu/',
 ]
 
 category_3 = [
